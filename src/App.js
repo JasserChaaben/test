bd7263e0b89d41a64c09c89f75f087269880d166
JasserChaaben <chaabnjasser@gmail.com>
2024-06-10 19:58:52 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import './App.css';
-
+import React, { useState } from 'react';
+const test = [
+  {
+    image:'./images/pizza.jpg',
+    name: 'Margherita Pizza',
+    ingredients: 'Tomato, Mozzarella, Basil',
+    price: '10DT'
+  },
+  {
+    image:'./images/salad.jpg',
+    name: 'Caesar Salad',
+    ingredients: 'Romaine, Parmesan, Croutons, Caesar Dressing',
+    price: '8,500DT'
+  },
+  {
+    image:'./images/spagetti.jpg',
+    name: 'Spaghetti Carbonara',
+    ingredients: 'Spaghetti, Eggs, Pecorino Romano, Pancetta, Pepper',
+    price: '6,500DT'
+  },
+  {
+    image:'./images/salmon.jpg',
+    name: 'Grilled Salmon',
+    ingredients: 'Salmon, Olive Oil, Lemon, Garlic, Dill',
+    price: '11,500DT'
+  },
+  {
+    image:'./images/Tiramisu.jpg',
+    name: 'Tiramisu',
+    ingredients: 'Mascarpone, Espresso, Ladyfingers, Cocoa Powder',
+    price: '7,500DT'
+  }
+];
 function App() {
   return (
     <div className="App">
-        
+          <div class="background-container"> <h1>NOM DE RESTAUT</h1> </div>
+          <div id="menu-box">
+          <Menu/>
+          </div>
+  <div class="content">
+  </div>
     </div>
   );
 }
+const Menu = () => {
+  const [items, setitems] = useState(test);
+  return (
+      <div id="menu">
+          {items.map((item) => (<Item pic={item.image} name={item.name} ingredients={item.ingredients} price={item.price}/>))}
+      </div>
+  );
+}
 
+const Item = (props) => {
+
+  return (
+      <div>
+        <div id="item">
+          <img id="picture" src={props.pic}></img>
+          <div id="details">
+          <h3>  {props.name}</h3>
+          <h6> {props.ingredients}</h6>
+          <h4>{props.price} </h4>
+          </div>
+          <button >Order</button>
+      </div>
+      </div>
+  );
+}
 export default App;
